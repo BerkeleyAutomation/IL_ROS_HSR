6d077508256f5be8bd44b52fb414f13854e6613d
DanielTakeshi <takeshidanny@gmail.com>
2018-11-18 12:07:12 -0800

    got net prediction there, next major step ...
    is to go from pixel to robot action. I might have to resort to what we were doing earlier with the tabel top code. Hopefully not

--- src/il_ros_hsr/nets/deploy_physical.py
+++ src/il_ros_hsr/nets/deploy_physical.py
@@ -1,13 +1,14 @@
 """For the PHYSICAL robot, tentative results.
 """
-import torch
-import argparse, copy, cv2, json, os, sys, time, pickle, logging
-import time, datetime
+import argparse, copy, cv2, json, os, sys, pickle
+import time, datetime, logging
 import numpy as np
 from os.path import join
 from collections import defaultdict
+import torch
+from torchvision import transforms
 
-import hsrb_interface
+import rospy, hsrb_interface
 from hsr_core.sensors import RGBD
 from hsr_core.utils import process_depth
 from il_ros_hsr.nets.net import ActPredictorNet
@@ -15,98 +16,99 @@ from il_ros_hsr.nets import options as opt
 from il_ros_hsr.nets import custom_transforms as CT
 
 
-###def transform_imgs(img_t, img_tp1, transform):
-###    """Designed for test-time inference.
-###    
-###    During training and validation, we have a class which works by taking pickle
-###    file with lists of the data in training / validation, and it processes them.
-###    But here, we assume we want to pass a new image to the model. The issue,
-###    however, is that the transforms that we use for validation (which we should
-###    use for test-time inference on new images with no labels) rely on assuming
-###    we have a target. So we will just add fictional targets.
-###
-###    Parameters
-###    ----------
-###    img_t, img_tp1:
-###        Two numpy arrays representing the two images for the network. We return
-###        their transformations.
-###    transform:
-###        Set of composed transforms that we used for VALIDATION.
-###    """
-###    assert img_t.shape == img_tp1.shape
-###
-###    # Fictional targets so that we can apply the same validation set transforms
-###    # Another way is to set transform classes to optionally ignore targets?
-###    target_xy  = [0.0, 0.0]
-###    target_l   = [0.0]
-###    target_ang = [1.0, 0.0, 0.0, 0.0]
-###
-###    # Feed through our transformations and return.
-###    sample = {
-###        'img_t':      img_t,
-###        'img_tp1':    img_tp1, 
-###        'target_xy':  target_xy,
-###        'target_l':   target_l,
-###        'target_ang': target_ang,
-###        'raw_ang':    -1,
-###    }
-###    sample = transform(sample)
-###    return sample
-###
-###
-###def deploy(act_predictor):
-###    """Let's see how to load just directly from two png images.
-###    
-###    In theory we should figure out what we can avoid from all the data loading
-###    machinery when we trained this network.
-###    """
-###    transforms_valid = transforms.Compose([
-###        CT.Rescale((256,256)),
-###        CT.CenterCrop((224,224)),
-###        CT.ToTensor(),
-###        CT.Normalize(opt.MEAN, opt.STD),
-###    ])
-###
-###    # Pick images to try here; careful, don't pick 'boundaries' of episodes.
-###    pth_t   = 'ssldata2/d_img_proc_10_003.png'
-###    pth_tp1 = 'ssldata2/d_img_proc_10_004.png'
-###    img_t   = cv2.imread(pth_t)
-###    img_tp1 = cv2.imread(pth_tp1)
-###
-###    t_input    = transform_imgs(img_t, img_tp1, transforms_valid)
-###    t_imgs_t   = t_input['img_t'].unsqueeze(0)
-###    t_imgs_tp1 = t_input['img_tp1'].unsqueeze(0)
-###
-###    out_pos, out_ang = act_predictor(t_imgs_t, t_imgs_tp1)
-###    _save_and_viz(pth_t, pth_tp1, img_t, img_tp1, out_pos, out_ang)
-###
-###
-###if __name__ == "__main__":
-###    # Pick the model that we want to load.
-###    HEAD  = '/nfs/diskstation/seita/bedmake_ssl'
-###    MODEL = 'resnet18_2018-11-18-09-50_000'
-###    PATH  = join(HEAD, MODEL, 'act_predictor.pt')
-###
-###    # Get old args we used, and put into a newer Namespace object.
-###    with open(join(HEAD, MODEL, 'args.json'), 'r') as fh:
-###        saved_args = json.load(fh)
-###    args = opt._json_to_args(jsonfile=saved_args)
-###
-###    # Load the pretrained model. If you print state dict, these start with
-###    # `layer` as that was the convention with the ResNet saved models.
-###    model = opt.get_pretrained_model(args)
-###
-###    # But now we use act predictor, with `pretrained_stem` in state dict.
-###    act_predictor = ActPredictorNet(model, args)
-###    #opt.debug_state_dict(model=act_predictor)
-###    act_predictor.load_state_dict(torch.load(PATH))
-###    act_predictor.eval()
-###
-###    deploy(act_predictor)
+def _save_and_viz(pth_t, pth_tp1, img_t, img_tp1, out_pos, out_ang):
+    """Really need to put much of this in `options.py` and reuse among scripts.
+    """
+    out_pos = out_pos.cpu().detach().numpy().squeeze()
+    if not os.path.exists(opt.DEPLOY_TMPDIR):
+        os.makedirs(opt.DEPLOY_TMPDIR)
+    b_pth_t   = os.path.basename(pth_t)
+    b_pth_tp1 = os.path.basename(pth_tp1)
+    print("base pth_t:   {}".format(b_pth_t))
+    print("base pth_tp1: {}".format(b_pth_tp1))
+    print("predicted position:     {}".format(out_pos))
+    print("predicted angle logits: {}".format(out_ang))
+
+    # The raw images
+    hstack1 = np.concatenate((img_t, img_tp1), axis=1)
+    fname1 = join(opt.DEPLOY_TMPDIR,'{}_1.png'.format(b_pth_t))
+    cv2.imwrite(fname1, hstack1)
+    print("Look at: {}".format(fname1))
+
+    # Transformed images, but _without_ the tensor and normalization stuff.
+    transforms_valid_notensors = transforms.Compose([
+        CT.Rescale((256,256)),
+        CT.CenterCrop((224,224)),
+    ])
+    t_input = transform_imgs(img_t, img_tp1, transforms_valid_notensors)
+    t_img_t   = t_input['img_t']
+    t_img_tp1 = t_input['img_tp1']
+    h, w, c = t_img_t.shape
+
+    # Save the _transformed_ images.
+    hstack2 = np.concatenate((t_img_t, t_img_tp1), axis=1)
+    fname2 = join(opt.DEPLOY_TMPDIR,'{}_2.png'.format(b_pth_t))
+    cv2.imwrite(fname2, hstack2)
+    print("Look at: {}".format(fname2))
+
+    # Overlay some stuff on the images (must de-process label, though).
+    pred_pos_int = int(out_pos[0]*w), int(out_pos[1]*h)
+    img = np.ascontiguousarray(t_img_t, dtype=np.uint8)
+    cv2.circle(img, center=pred_pos_int, radius=2, color=opt.BLUE,  thickness=-1)
+    cv2.circle(img, center=pred_pos_int, radius=3, color=opt.GREEN, thickness=1)
+    cv2.putText(img=img, 
+                text="pred pos: {}".format(pred_pos_int),
+                org=(10,15),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX, 
+                fontScale=0.5, 
+                color=opt.GREEN,
+                thickness=1)
+
+    # Save _transformed_ images, WITH predicted stuff overlaid on it.
+    hstack3 = np.concatenate((img, t_img_tp1), axis=1)
+    fname3 = join(opt.DEPLOY_TMPDIR,'{}_3.png'.format(b_pth_t))
+    cv2.imwrite(fname3, hstack3)
+    print("Look at: {}".format(fname3))
+    print("PS, t_img_t.shape: {}".format(t_img_t.shape))
 
 
+def transform_imgs(img_t, img_tp1, transform):
+    """Designed for test-time inference.
+    
+    During training and validation, we have a class which works by taking pickle
+    file with lists of the data in training / validation, and it processes them.
+    But here, we assume we want to pass a new image to the model. The issue,
+    however, is that the transforms that we use for validation (which we should
+    use for test-time inference on new images with no labels) rely on assuming
+    we have a target. So we will just add fictional targets.
 
+    Parameters
+    ----------
+    img_t, img_tp1:
+        Two numpy arrays representing the two images for the network. We return
+        their transformations.
+    transform:
+        Set of composed transforms that we used for VALIDATION.
+    """
+    assert img_t.shape == img_tp1.shape
 
+    # Fictional targets so that we can apply the same validation set transforms
+    # Another way is to set transform classes to optionally ignore targets?
+    target_xy  = [0.0, 0.0]
+    target_l   = [0.0]
+    target_ang = [1.0, 0.0, 0.0, 0.0]
+
+    # Feed through our transformations and return.
+    sample = {
+        'img_t':      img_t,
+        'img_tp1':    img_tp1, 
+        'target_xy':  target_xy,
+        'target_l':   target_l,
+        'target_ang': target_ang,
+        'raw_ang':    -1,
+    }
+    sample = transform(sample)
+    return sample
 
 
 # Will put in `options.py` later
@@ -149,9 +151,8 @@ class DataCollector:
         """Get a series of target images, stopping only when user wishes to
         abort (via pressing ESC key).
 
-        Note, in phase 1, we will just add onto existing indices in saved target
-        dir. We assume that we can sort these, then extract the index from the
-        last element.
+        Note, in this phase, we add onto existing indices in saved target dir.
+        We assume we can sort these, then extract the index from the last item.
         """
         args = self.args
         if not os.path.exists(TARGET_DIR):
@@ -190,22 +191,90 @@ class DataCollector:
                 idx += 1
 
 
+    def deploy(self):
+        """Deploy!! Careful, did you run the first phases beforehand?
+        """
+        # Pick the model that we want to load.
+        HEAD  = '/nfs/diskstation/seita/bedmake_ssl'
+        MODEL = 'resnet18_2018-11-18-09-50_000'
+        PATH  = join(HEAD, MODEL, 'act_predictor.pt')
+        
+        # Get old args we used, and put into a newer Namespace object.
+        with open(join(HEAD, MODEL, 'args.json'), 'r') as fh:
+            saved_args = json.load(fh)
+        args = opt._json_to_args(jsonfile=saved_args)
+        
+        # Load the pretrained model. If you print state dict, these start with
+        # `layer` as that was the convention with the ResNet saved models.
+        model = opt.get_pretrained_model(args)
+        
+        # But now we use act predictor, with `pretrained_stem` in state dict.
+        act_predictor = ActPredictorNet(model, args)
+        act_predictor.load_state_dict(torch.load(PATH))
+        act_predictor.eval()
+
+        transforms_valid = transforms.Compose([
+            CT.Rescale((256,256)),
+            CT.CenterCrop((224,224)),
+            CT.ToTensor(),
+            CT.Normalize(opt.MEAN, opt.STD),
+        ])
+        
+        # Pick images to try from the saved file.
+        pth_end = 'tmp_physical_targs/d_img_proc_001.png'
+        img_end = cv2.imread(pth_end)
+
+        # Get current image 
+        c_img, d_img, d_img_proc = self.get_images()
+
+        caption = 'ESC to abort.'
+        img_t = d_img_proc
+        hstack = np.concatenate((img_t, img_end), axis=1)
+        opt.call_wait_key( cv2.imshow(caption, hstack) )
+
+        t_input   = transform_imgs(img_t, img_end, transforms_valid)
+        t_img_t   = t_input['img_t'].unsqueeze(0)    # after unsqueeze these are
+        t_img_end = t_input['img_tp1'].unsqueeze(0)  # both shape (1,3,224,224)
+        out_pos, out_ang = act_predictor(t_img_t, t_img_end)
+
+        w, h = 224, 224
+        print(out_pos)
+        print(out_ang)
+        print(t_img_t.shape)
+        print(t_img_end.shape)
+        out_pos = out_pos.cpu().detach().squeeze()
+        out_ang = out_ang.cpu().detach().squeeze()
+        pred_pos_int = int(out_pos[0]*w), int(out_pos[1]*h)
+        print(pred_pos_int)
+        #_save_and_viz(pth_t, pth_end, img_t, img_end, out_pos, out_ang)
+
+
 if __name__ == "__main__":
     # --------------------------------------------------------------------------
     pp = argparse.ArgumentParser()
     pp.add_argument('--phase', type=int, help='see code and README docs')
     args = pp.parse_args()
-    assert args.phase in [1,2]
+    assert args.phase in [0,1,2]
     # --------------------------------------------------------------------------
 
     dc = DataCollector(args)
     dc.orient_robot()
 
-    if args.phase == 1:
+    if args.phase == 0:
         # First, focus on getting a set of target images for the robot. Easy to
         # do it here because we assume that once we're done the robot is in the
         # same position for the next phase (trying to grasp and pull) so we do
         # not have to worry about position and orientation issues.
         dc.get_target_images()
+    elif args.phase == 1:
+        # Ensure that the poses look good. Start up rviz and look at the map
+        # pose. Ideally we have a better way of doing this, though ... this
+        # serves the same purpose as phase 1 in my earlier code.
+        print("Look at rviz and check that poses are good.")
+        print("E.g., robot base should be facing in positive x-axis direction")
+        print("which should be aligned with the long edge of the bed.")
+        rospy.spin()
+    elif args.phase == 2:
+        dc.deploy()
     else:
         raise NotImplementedError()
